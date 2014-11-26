7e814ff4a54ef6d691fbc7474f2f8e6d9a5bc513
Mattia Sorgato <mattia.sorgato@gmail.com>
2014-11-26 15:50:40 +0100

    Costruisco i controlli per l'input
    Ora manca da realizzare un test per ogni controllo

--- src/Parser/ConcreteParser.java
+++ src/Parser/ConcreteParser.java
@@ -6,9 +6,12 @@ public class ConcreteParser implements IParser {
 	public boolean parseLine(String line) {
 		String[] parts = line.split("\t");			//divido gli elementi della stringa splittando sul carattere di tabulazione
 		for(int i = 0; i < parts.length; i++)
-			parts[i] = parts[i].trim();				//elimino eventuali spazi bianchi
+			if(i != 1)
+				parts[i] = parts[i].trim();			//elimino eventuali spazi bianchi, ma non nel carattere del pezzo
 		if(parts[1].length() > 1)
 			return false;							//il carattere del pezzo non può essere più lungo di una lettera
+		if(parts.length != 6)
+			return false;							//il numero di elementi della Stringa dev'essere obbligatoriamente 6 (id, char, id_n, id_e, id_s, id_w)
 		return true;
 		
 	}
