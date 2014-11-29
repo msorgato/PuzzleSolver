ad0290b3517e99bc601d1913469bc8c9f80e0ed8
Mattia Sorgato <mattia.sorgato@gmail.com>
2014-11-29 17:57:15 +0100

    Abbozzo il metodo di lettura

--- src/Parser/IParser.java
+++ src/Parser/IParser.java
@@ -1,7 +1,7 @@
 ﻿package Parser;
 
 public interface IParser {
-	//parser che stabilisce se una stringa sialecita o meno
+	//parser che stabilisce se una stringa sia lecita o meno
 	public boolean parseLine(String line);		
 	
 	//parser che ritorna -1 se il puzzle e' lecito, i in [0 .. puzzle.length] se un pezzo non matcha il pattern voluto
---
+++ src/PuzzleSolver.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import Parser.IParser;
+import Parser.PuzzleParser;
+
+
+public class PuzzleSolver {
+	
+	private static Charset charset = StandardCharsets.UTF_8;
+	private List<String> puzzle;
+
+	private static List<String> readContent(Path inputPath) {
+		List<String> content = new ArrayList<String>();
+		try (BufferedReader reader = Files.newBufferedReader(inputPath,	charset)) {
+			String line = null;
+			IParser parser = new PuzzleParser();
+			while ((line = reader.readLine()) != null) {
+				if(!parser.parseLine(line))
+					return null;
+				content.add(line);
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		return content;
+	}
+		
+	private static void writeContent(Path file, String content) {
+		try (BufferedWriter writer = Files.newBufferedWriter(file, charset)) {
+			writer.write(content);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
