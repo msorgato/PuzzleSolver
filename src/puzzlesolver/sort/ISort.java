29b022da3c9c6d1de66287b90e61e8df0aa981ac
Mattia Sorgato <mattia.sorgato@gmail.com>
2014-12-03 17:55:36 +0100

    Struttura base dell'algoritmo completata
    Ora bisogna testarla e ritestarla =_= :facepunch:

--- src/puzzlesolver/sort/ISort.java
+++ src/puzzlesolver/sort/ISort.java
@@ -5,6 +5,8 @@ import java.util.List;
 import puzzlesolver.piece.Piece;
 
 public interface ISort {
+	Piece removePiece(String id, List<Piece> puzzle);
+	
 	List<Piece> sortLine(Piece firstPiece, List<Piece> puzzle);
 	
 	Piece[][] sortPuzzle(List<Piece> puzzle);
--- src/puzzlesolver/sort/SequentialSort.java
+++ src/puzzlesolver/sort/SequentialSort.java
@@ -7,6 +7,13 @@ import puzzlesolver.piece.Piece;
 
 public class SequentialSort implements ISort {
 	
+	public Piece removePiece(String id, List<Piece> puzzle) {
+		for(int i = 0; i < puzzle.size(); i++)
+			if(puzzle.get(i).getId().equals(id)) 
+				return puzzle.remove(i);
+		return null;											//se non trovo il pezzo, ritorno null
+	}
+	
 	public List<Piece> sortLine(Piece firstPiece, List<Piece> puzzle) {
 		Piece currentPiece = firstPiece;
 		int currentIndex = 0;
@@ -42,12 +49,27 @@ public class SequentialSort implements ISort {
 		if(firstLine == null)	//mancano pezzi nella prima riga, i suppose
 			return null;
 		
-		Piece[][] orderedPuzzle = new Piece[puzzleSize / firstLine.size()][firstLine.size()];	//QUESTA RIGA POTREBBE ESSERE UNA PUTTANATA INCREDIBILE.
+		int rows = puzzleSize / firstLine.size();	
+		int cols = firstLine.size();									//QUESTE RIGHE POTREBBERO
+		Piece[][] orderedPuzzle = new Piece[rows][cols];	//ESSERE UNA PUTTANATA INCREDIBILE
+		for(int i = 0; i < cols; i++)
+			orderedPuzzle[0][i] = firstLine.get(i);
 		
 		/*
 		 * da qua in poi, si ordina ogni riga sequenzialmente e si prende il sud di ogni primo elemento
 		 * come elemento iniziale della riga successiva. And so on.
 		 */
+		
+		for(int i = 1; i < rows; i++) {
+			Piece firstRowPiece = this.removePiece(orderedPuzzle[i - 1][0].getSouth(), puzzle);
+			if(firstRowPiece == null)
+				return null;
+			List<Piece> rowPieces = this.sortLine(firstRowPiece, puzzle);
+			if(rowPieces == null)
+				return null;
+			for(int j = 0; j < cols; j++)
+				orderedPuzzle[i][j] = rowPieces.get(j);
+		}
 		
 		
 		
