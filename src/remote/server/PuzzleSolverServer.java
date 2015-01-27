814c4ecbd67d3df6e6a5e0a06aea41a5bdaa27c1
Mattia Sorgato <mattia.sorgato@gmail.com>
2015-01-27 12:16:20 +0100

    Prova del Server

--- src/remote/server/PuzzleSolverServer.java
+++ src/remote/server/PuzzleSolverServer.java
@@ -1,9 +1,29 @@
 package remote.server;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
 public class PuzzleSolverServer {
 
 	public static void main(String[] args) {
-		
+		ConcreteRemoteSolver solver = null;
+		try {
+			solver = new ConcreteRemoteSolver();
+		} catch (RemoteException e) {
+			System.out.println("Non crea il solver remoto.");
+			e.printStackTrace();
+		}
+			try {
+				Naming.rebind("rmi://localhost/R", solver);
+			} catch (RemoteException e) {
+				System.out.println("Lancia RemoteException");
+				e.printStackTrace();
+			} catch (MalformedURLException e) {
+				System.out.println("Lancia MalformedURLException");
+				e.printStackTrace();
+			}
+		System.out.println("Server Partito!");
 
 	}
 
